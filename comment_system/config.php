376a979a7b263a286926594bab9f1797cc96b42e
zdnpl <83897266+zdnpl@users.noreply.github.com>
2021-09-16 13:56:39 +0700

    Update config.php

--- comment_system/config.php
+++ comment_system/config.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "sql308.epizy.com";
-    $username = "epiz_29500431";
-    $password = "zdnpladmin";
-    $database = "epiz_29500431_comment";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "comment";
 
     $conn = mysqli_connect($servername, $username, $password, $database);
 
